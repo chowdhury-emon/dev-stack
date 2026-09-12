46f40dacbd2d1a14109b3e5ab9bd77f962810c7a
Md Emon Chowdhury <emonc.dev@gmail.com>
2026-09-12 22:28:05 +0600

    Hero: fixed UI buggs

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,21 +2,21 @@ import bannerStackImage from './../../assets/banner-stack.png'
 
 function Hero() {
     return (
-        <div className="container mx-auto px-4 grid gird-col-1 md:grid-cols-2 items-center">
+        <div className="container mx-auto px-4 grid gird-col-1 md:grid-cols-2 items-center mt-15 md:mt-auto">
             {/* Hero Details Column */}
-            <div className="grid items-center gap-6 text-center md:text-left">
-                <h1 className="text-3xl md:text-5xl font-black text-black mt-20 md:mt-auto">
+            <div className="grid items-center gap-6 text-center md:text-left ">
+                <h1 className="text-3xl md:text-5xl font-black text-black">
                     <div >Build Your Ideal</div>
                     <div className="w-fit mx-auto md:mx-0 bg-linear-to-r from-secondary to-[#8338E4] bg-clip-text text-transparent ">Development Stack</div>
                 </h1>
 
-                <p className='text-balance'>
+                <p className="text-pretty">
                     Explore frontend, backend, database, and tooling options,
                     compare them side by side, and put together the stack that fits your
                     next project.
                 </p>
 
-                <div className="w-fit mx-auto md:mx-0 grid grid-cols-2 gap-4 mt-8 border-red-500">
+                <div className="w-fit mx-auto md:mx-0 grid grid-cols-2 gap-2 mt-8 border-red-500">
                     <button
                         className="p-2 px-3 text-sm rounded-lg border-2 border-neutral-100 bg-linear-to-r from-secondary to-primary text-white">
                         Explore Technologies
