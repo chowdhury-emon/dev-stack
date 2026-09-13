39b557ced8c73e10e60e8be33bdf5c1145f08e70
Md Emon Chowdhury <emonc.dev@gmail.com>
2026-09-13 18:44:47 +0600

    Footer: fixed bottom link bug

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -46,7 +46,7 @@ function Footer() {
                         </div>
                     </div>
                 </section>
-                <section className="flex justify-between py-10 pb-15 border-t border-neutral-100">
+                <section className="flex justify-between py-10 pb-15 border-t border-neutral-100 text-xs">
                     <p>© 2026 Dev Stack. All rights reserved.</p>
                     <div className="flex gap-2">
                         <a href="#">Privecy</a>
